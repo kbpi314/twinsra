41f98f21658a8a8bd5bf9bd63ed9c3c95f5b49de
Kevin Bu <kbpi314@gmail.com>
2025-02-02 17:05:37 -0500

    updated twins uk compariosn

--- code/glm.R
+++ code/glm.R
@@ -7,3 +7,17 @@ summary(model)
 
 model <- lm(formula = Bray_Curtis ~ Abx_diff + smoking.previously_diff + Alcohol.current_diff + NSAIDS.current_diff + NSAIDS.last.3.mo_diff, data=df)
 summary(model)
+
+
+# 
+library(lme4)
+df <- read.csv('/Users/KevinBu/Desktop/clemente_lab/Projects/twinsra/inputs/df_lmer.csv')
+
+
+# glms
+model <- lm(formula = shannon_entropy_diff ~ MTX, data=df)
+summary(model)
+
+# glms
+model <- lm(formula = Bray_Curtis ~ MTX, data=df)
+summary(model)
